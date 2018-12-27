abd7c296f8e28dee4a28372b46b05eb81cfeeb95
rob miller <robert.miller1@bhpbilliton.com>
2018-12-27 17:34:02 +0800

    Added aliases for project locations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,7 @@
 source ~/.config/fish/.credentials
 
 alias ve="source ./venv/bin/activate.fish"
+alias cdc="cd ~/work/dev/crew_rostering"
+alias cda="cd ~/work/dev/apps-domain-cloudfront"
+alias cdd="cd ~/config/"
+
