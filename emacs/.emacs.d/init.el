ed00da626f09081675aa6cac2f8b027bd66e0d74
rob miller <vasdee@localhost.localdomain>
2022-08-11 10:22:19 +0800

    update
    * basic terraform mode added
    * snippets for python
    * ssh config with more jump hosts
    * aws config for SW accounts

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -82,6 +82,11 @@ inhibit-startup-echo-area-message t)
 ;; Package configurations
 ;; ---------------------------------------------------------
 
+(use-package terraform-mode
+  :ensure t
+)
+
+
 (use-package dashboard
  :ensure t
  :config
@@ -192,7 +197,7 @@ inhibit-startup-echo-area-message t)
       (:eval
        (let ((project-name (projectile-project-name)))
          (unless (string= "-" project-name)
-           (format " in [%s]" project-name))))))
+           (format " [%s]" project-name))))))
 )
 
 ;; Fix the indenting issues in emacs
@@ -622,7 +627,7 @@ inhibit-startup-echo-area-message t)
    '("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" "2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" "3f44e2d33b9deb2da947523e2169031d3707eec0426e78c7b8a646ef773a2077" "aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" "a4df5d4a4c343b2712a8ed16bc1488807cd71b25e3108e648d4a26b02bc990b3" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
  '(js-indent-level 4)
  '(package-selected-packages
-   '(dap-python sql-mode realgud svelte-mode svelt-mode color-theme-sanityinc-tomorrow csv-mode dash dap-mode makefile-executor omnisharp typescript-mode dashboard magit-popup neotree nord-theme projectile spacemacs-theme move-text aggressive-indent csharp-mode restclient x509-mode powershell all-the-icons-dired lsp-elixir flycheck-prospector doom-modeline docker-compose-mode use-package company-lsp lsp-python lsp-ui lsp-mode dockerfile-mode add-node-modules-path all-the-icons counsel json-mode yaml-mode ag magit fish-mode markdown-mode rjsx-mode dracula-theme yasnippet-snippets js2-mode web-mode flycheck))
+   '(terraform-mode dap-python sql-mode realgud svelte-mode svelt-mode color-theme-sanityinc-tomorrow csv-mode dash dap-mode makefile-executor omnisharp typescript-mode dashboard magit-popup neotree nord-theme projectile spacemacs-theme move-text aggressive-indent csharp-mode restclient x509-mode powershell all-the-icons-dired lsp-elixir flycheck-prospector doom-modeline docker-compose-mode use-package company-lsp lsp-python lsp-ui lsp-mode dockerfile-mode add-node-modules-path all-the-icons counsel json-mode yaml-mode ag magit fish-mode markdown-mode rjsx-mode dracula-theme yasnippet-snippets js2-mode web-mode flycheck))
  '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
  '(sgml-basic-offset 4))
 (custom-set-faces
