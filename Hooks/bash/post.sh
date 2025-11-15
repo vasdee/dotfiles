e936edcb07cead32b798ebec7d65414b597c8203
rob miller <robert.miller1@bhp.com>
2025-11-15 09:09:46 +0800

    * added post hook bash script to enable custom bashrcs

--- Hooks/bash/post.sh
+++ Hooks/bash/post.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+echo "Attempting to run bash post install script"
 
-echo ". ~/.bashrc-local.bash" >> ~/.bashrc
+SOURCE_LOCAL_BASHRC_CMD=". ~/.bashrc-local.bash"
+
+if ! grep -q "$SOURCE_LOCAL_BASHRC_CMD" ~/.bashrc; then
+    echo "Adding $SOURCE_LOCAL_BASHRC_CMD local bashrc for to .bashrc"
+    echo "$SOURCE_LOCAL_BASHRC_CMD" >> ~/.bashrc
+fi
+
