27177accdbf35e4d9b309d92834877f829912c63
rob miller <funky.style@gmail.com>
2026-01-12 16:57:06 +0800

    add cert suffixes during bootstrap
    * pre-create some dirs in the install

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 CLONE_URL=${CLONE_URL:="https://github.com/vasdee/dotfiles.git"}
 LOCAL_CLONE_PATH=${LOCAL_CLONE_PATH:=~/.config/dotfiles}
 DOTFILES_TAG=""
+LOCAL_BIN="$HOME/.local/bin"
 
 echo "Starting dotfile install"
 
 echo "Cloning $CLONE_URL to $CLONE_DIR"
-mkdir -p $LOCAL_CLONE_PATH
+mkdir -p $LOCAL_CLONE_PATH $LOCAL_BIN
 
 GIT_OPTIONS=""
 if [ -n "${DOTFILES_TAG}" ]; then
@@ -15,11 +16,8 @@ if [ -n "${DOTFILES_TAG}" ]; then
 fi
 git clone $GIT_OPTIONS $CLONE_URL $LOCAL_CLONE_PATH
 
-cd $LOCAL_CLONE_PATH
-
 echo "linking tuckr to ~/.local/bin/tuckr - make sure this is set in your path or run bash or zsh hooks"
-ln -s $PWD/bin/$(uname -s)/$(uname -m)/tuckr ~/.local/bin/tuckr
-LOCAL_BIN="$HOME/.local/bin"
+ln -s $LOCAL_CLONE_PATH/bin/$(uname -s)/$(uname -m)/tuckr ~/.local/bin/tuckr
 
 # Add the path temporarily
 export PATH=$LOCAL_BIN:$PATH
