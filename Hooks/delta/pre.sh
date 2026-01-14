d3812f95d2b2b47482c25effcb11ba9adf5ae43f
sthysel <sthysel@gmail.com>
2026-01-14 14:42:45 +0800

    feat: adds git-delta

---
+++ Hooks/delta/pre.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+. ../../lib/lib.sh
+
+fedora() {
+    rootdo dnf install -y git-delta
+}
+
+debian() {
+    rootdo apt install -y git-delta
+}
+
+ubuntu() {
+    debian
+}
+
+steamos() {
+    rootdo pacman -S --noconfirm git-delta
+}
+
+macos() {
+    brew install git-delta
+}
+
+linux() {
+    # Fallback for Linux distros without package manager support
+    LATEST_VERSION=$(github_latest_tag "dandavison/delta")
+    VERSION=${DELTA_VERSION:=$LATEST_VERSION}
+    FILENAME="delta-${VERSION}-$(uname -m)-unknown-linux-gnu.tar.gz"
+    curl -fOL "https://github.com/dandavison/delta/releases/download/${VERSION}/${FILENAME}"
+    tar -xzf ${FILENAME}
+    mv delta-${VERSION}-$(uname -m)-unknown-linux-gnu/delta ~/.local/bin/
+    chmod +x ~/.local/bin/delta
+}
