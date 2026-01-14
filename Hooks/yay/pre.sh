b7336709e37051a335af1fdd4ea4817a0331cc03
sthysel <sthysel@gmail.com>
2026-01-14 16:58:51 +0800

    fix: depend on wrapper to clean up

--- Hooks/yay/pre.sh
+++ Hooks/yay/pre.sh
@@ -5,12 +5,8 @@
 arch() {
     rootdo pacman -S --needed --noconfirm git base-devel
     if ! command -v yay >/dev/null 2>&1; then
-        TMPDIR=$(mktemp -d)
-        git clone https://aur.archlinux.org/yay.git "$TMPDIR/yay"
-        cd "$TMPDIR/yay"
+        git clone https://aur.archlinux.org/yay.git yay
         makepkg -si --noconfirm
-        cd -
-        rm -rf "$TMPDIR"
     fi
 }
 
