8d9c46fd06e3f2a44b30347fb537013dcb5bd6f9
rob miller <funky.style@gmail.com>
2026-01-22 10:14:26 +0800

    Simplifying readme

--- bash/config/.bashrc.d/caifs-config.bash
+++
@@ -1,2 +0,0 @@
-
-export CAIFS_COLLECTIONS="/home/millrt9/code/private/github.com/vasdee/caifs:/home/millrt9/code/private/github.com/vasdee/dotfiles:/home/millrt9/code/private/github.com/vasdee/caifs-common"
